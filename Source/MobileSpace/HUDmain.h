ed516618da61a5c334598ad41b05a2cf8ddbd0c3
Atzelcer <atzelcer@gmail.com>
2025-10-08 16:46:00 -0400

    principal

---
+++ Source/MobileSpace/HUDmain.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "HUDmain.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MOBILESPACE_API AHUDmain : public AHUD
+{
+	GENERATED_BODY()
+	
+};
