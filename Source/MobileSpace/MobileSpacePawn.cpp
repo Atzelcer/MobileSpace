177f3694a021a7fe578d4776ef021ce633477cc1
VelaGutierrezElmer-progra <elmerve77@gmail.com>
2025-10-08 22:54:35 -0400

    pawn and camara component atached map

--- Source/MobileSpace/MobileSpacePawn.cpp
+++ Source/MobileSpace/MobileSpacePawn.cpp
@@ -20,32 +20,22 @@ const FName AMobileSpacePawn::FireRightBinding("FireRight");
 
 AMobileSpacePawn::AMobileSpacePawn()
 {	
-	static ConstructorHelpers::FObjectFinder<UStaticMesh> ShipMesh(TEXT("/Game/TwinStick/Meshes/TwinStickUFO.TwinStickUFO"));
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> ShipMesh(TEXT("StaticMesh'/Game/StarSparrow/Meshes/Examples/SM_StarSparrow19.SM_StarSparrow19'"));
 	// Create the mesh component
 	ShipMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ShipMesh"));
 	RootComponent = ShipMeshComponent;
 	ShipMeshComponent->SetCollisionProfileName(UCollisionProfile::Pawn_ProfileName);
 	ShipMeshComponent->SetStaticMesh(ShipMesh.Object);
+	ShipMeshComponent->SetRelativeScale3D(FVector(0.1f, 0.1f, 0.1f));
 	
 	// Cache our sound effect
 	static ConstructorHelpers::FObjectFinder<USoundBase> FireAudio(TEXT("/Game/TwinStick/Audio/TwinStickFire.TwinStickFire"));
 	FireSound = FireAudio.Object;
 
-	// Create a camera boom...
-	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
-	CameraBoom->SetupAttachment(RootComponent);
-	CameraBoom->SetUsingAbsoluteRotation(true); // Don't want arm to rotate when ship does
-	CameraBoom->TargetArmLength = 1200.f;
-	CameraBoom->SetRelativeRotation(FRotator(-80.f, 0.f, 0.f));
-	CameraBoom->bDoCollisionTest = false; // Don't want to pull camera in when it collides with level
-
-	// Create a camera...
-	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("TopDownCamera"));
-	CameraComponent->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
-	CameraComponent->bUsePawnControlRotation = false;	// Camera does not rotate relative to arm
+	// No camera setup - using fixed camera from GameMode
 
 	// Movement
-	MoveSpeed = 1000.0f;
+	MoveSpeed = 1800.0f;
 	// Weapon
 	GunOffset = FVector(90.f, 0.f, 0.f);
 	FireRate = 0.1f;
@@ -65,38 +55,60 @@ void AMobileSpacePawn::SetupPlayerInputComponent(class UInputComponent* PlayerIn
 
 void AMobileSpacePawn::Tick(float DeltaSeconds)
 {
-	// Find movement direction
-	const float ForwardValue = GetInputAxisValue(MoveForwardBinding);
-	const float RightValue = GetInputAxisValue(MoveRightBinding);
+	// Galaga-style controls: Movement in both axes but no rotation
+	const float ForwardValue = GetInputAxisValue(MoveForwardBinding);  // W/S movement
+	const float RightValue = GetInputAxisValue(MoveRightBinding);      // A/D movement
 
-	// Clamp max size so that (X=1, Y=1) doesn't cause faster movement in diagonal directions
-	const FVector MoveDirection = FVector(ForwardValue, RightValue, 0.f).GetClampedToMaxSize(1.0f);
+	// Create movement in both X (forward/back) and Y (left/right) axes
+	const FVector Movement = FVector(ForwardValue, RightValue, 0.f) * MoveSpeed * DeltaSeconds;
 
-	// Calculate  movement
-	const FVector Movement = MoveDirection * MoveSpeed * DeltaSeconds;
+	// Get current rotation once to avoid duplicate declarations
+	const FRotator CurrentRotation = GetActorRotation();
+	FRotator SmoothRotation;
+
+	// Banking effect: Soft tilt the ship based on horizontal movement
+	if (FMath::Abs(RightValue) > 0.0f)
+	{
+		// Apply banking when moving horizontally
+		const float BankingAngle = RightValue * 15.0f; // Maximum 15 degrees of banking (more subtle)
+		const FRotator TargetRotation = FRotator(0.f, 0.f, -BankingAngle); // Roll rotation for banking
+		SmoothRotation = FMath::RInterpTo(CurrentRotation, TargetRotation, DeltaSeconds, 5.0f);
+	}
+	else
+	{
+		// If not moving horizontally, gradually return to neutral position
+		const FRotator NeutralRotation = FRotator(0.f, 0.f, 0.f);
+		SmoothRotation = FMath::RInterpTo(CurrentRotation, NeutralRotation, DeltaSeconds, 4.0f);
+	}
+	
+	// Apply the smooth rotation
+	SetActorRotation(SmoothRotation);
 
 	// If non-zero size, move this actor
 	if (Movement.SizeSquared() > 0.0f)
 	{
-		const FRotator NewRotation = Movement.Rotation();
 		FHitResult Hit(1.f);
-		RootComponent->MoveComponent(Movement, NewRotation, true, &Hit);
+		RootComponent->MoveComponent(Movement, SmoothRotation, true, &Hit);
 		
 		if (Hit.IsValidBlockingHit())
 		{
 			const FVector Normal2D = Hit.Normal.GetSafeNormal2D();
 			const FVector Deflection = FVector::VectorPlaneProject(Movement, Normal2D) * (1.f - Hit.Time);
-			RootComponent->MoveComponent(Deflection, NewRotation, true);
+			RootComponent->MoveComponent(Deflection, SmoothRotation, true);
 		}
 	}
 	
-	// Create fire direction vector
+	// Galaga-style shooting: Check for fire input (any fire input shoots upward)
 	const float FireForwardValue = GetInputAxisValue(FireForwardBinding);
 	const float FireRightValue = GetInputAxisValue(FireRightBinding);
-	const FVector FireDirection = FVector(FireForwardValue, FireRightValue, 0.f);
-
-	// Try and fire a shot
-	FireShot(FireDirection);
+	
+	// If any fire input is detected, shoot forward (upward in Galaga style)
+	if (FMath::Abs(FireForwardValue) > 0.0f || FMath::Abs(FireRightValue) > 0.0f)
+	{
+		// Always shoot forward (X direction)
+		const FVector FireDirection = FVector(1.f, 0.f, 0.f);
+		FireShot(FireDirection);
+	}
 }
 
 void AMobileSpacePawn::FireShot(FVector FireDirection)
@@ -104,30 +116,26 @@ void AMobileSpacePawn::FireShot(FVector FireDirection)
 	// If it's ok to fire again
 	if (bCanFire == true)
 	{
-		// If we are pressing fire stick in a direction
-		if (FireDirection.SizeSquared() > 0.0f)
+		// Always fire forward in Galaga style
+		const FRotator FireRotation = FVector(1.f, 0.f, 0.f).Rotation(); // Always forward
+		
+		// Spawn projectile at an offset from this pawn
+		const FVector SpawnLocation = GetActorLocation() + FireRotation.RotateVector(GunOffset);
+
+		UWorld* const World = GetWorld();
+		if (World != nullptr)
 		{
-			const FRotator FireRotation = FireDirection.Rotation();
-			// Spawn projectile at an offset from this pawn
-			const FVector SpawnLocation = GetActorLocation() + FireRotation.RotateVector(GunOffset);
+			// spawn the projectile
+			World->SpawnActor<AMobileSpaceProjectile>(SpawnLocation, FireRotation);
+		}
 
-			UWorld* const World = GetWorld();
-			if (World != nullptr)
-			{
-				// spawn the projectile
-				World->SpawnActor<AMobileSpaceProjectile>(SpawnLocation, FireRotation);
-			}
+		bCanFire = false;
+		World->GetTimerManager().SetTimer(TimerHandle_ShotTimerExpired, this, &AMobileSpacePawn::ShotTimerExpired, FireRate);
 
-			bCanFire = false;
-			World->GetTimerManager().SetTimer(TimerHandle_ShotTimerExpired, this, &AMobileSpacePawn::ShotTimerExpired, FireRate);
-
-			// try and play the sound if specified
-			if (FireSound != nullptr)
-			{
-				UGameplayStatics::PlaySoundAtLocation(this, FireSound, GetActorLocation());
-			}
-
-			bCanFire = false;
+		// try and play the sound if specified
+		if (FireSound != nullptr)
+		{
+			UGameplayStatics::PlaySoundAtLocation(this, FireSound, GetActorLocation());
 		}
 	}
 }
--- Source/MobileSpace/MobileSpacePawn.h
+++ Source/MobileSpace/MobileSpacePawn.h
@@ -15,13 +15,14 @@ class AMobileSpacePawn : public APawn
 	UPROPERTY(Category = Mesh, VisibleDefaultsOnly, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
 	class UStaticMeshComponent* ShipMeshComponent;
 
-	/** The camera */
-	UPROPERTY(Category = Camera, VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
-	class UCameraComponent* CameraComponent;
+	// Camera components removed - using fixed camera from GameMode
+	// /** The camera */
+	// UPROPERTY(Category = Camera, VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	// class UCameraComponent* CameraComponent;
 
-	/** Camera boom positioning the camera above the character */
-	UPROPERTY(Category = Camera, VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
-	class USpringArmComponent* CameraBoom;
+	// /** Camera boom positioning the camera above the character */
+	// UPROPERTY(Category = Camera, VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	// class USpringArmComponent* CameraBoom;
 
 public:
 	AMobileSpacePawn();
@@ -70,9 +71,10 @@ private:
 public:
 	/** Returns ShipMeshComponent subobject **/
 	FORCEINLINE class UStaticMeshComponent* GetShipMeshComponent() const { return ShipMeshComponent; }
-	/** Returns CameraComponent subobject **/
-	FORCEINLINE class UCameraComponent* GetCameraComponent() const { return CameraComponent; }
-	/** Returns CameraBoom subobject **/
-	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+	// Camera getters removed - using fixed camera from GameMode
+	// /** Returns CameraComponent subobject **/
+	// FORCEINLINE class UCameraComponent* GetCameraComponent() const { return CameraComponent; }
+	// /** Returns CameraBoom subobject **/
+	// FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
 };
 
